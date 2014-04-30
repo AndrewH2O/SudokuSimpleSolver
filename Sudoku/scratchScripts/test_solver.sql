b471e6147c5a9d294cc435d443d6916a3c9c0081
andrew w8 <u@eg.com>
2014-04-30 18:50:14 +0100

    updated test_solver.sql to repeatedly run either until a solution has been found or no further digits have been found

--- Sudoku/scratchScripts/test_solver.sql
+++ Sudoku/scratchScripts/test_solver.sql
@@ -1,4 +1,4 @@
-﻿--use Sudoku
+﻿use Sudoku
 --DECLARE @result int;
 --exec  @result = sp_rule_nakedSingle;
 --if(@result<>-1) 
@@ -16,70 +16,91 @@
 --select * from candidate_digits where cellID=4
 --select * from candidate_digits where cellID=65
 
+
+
 --exec sp_rule_nakedSingle
-DECLARE @result int;
+DECLARE @result int, @hasFound bit, @isSoln bit;
 
+set @hasFound=1;
+set @isSoln=0;
 
-set @result=0;
-while @result<>-1
+while ( @isSoln=0 and @hasFound=1)
 begin
-	exec @result = sp_rule_nakedSingle;
-	if(@result<>-1) 
+	
+	set @hasFound=0;
+	set @result=0;
+	while @result<>-1
 	begin
-		select 'rule "naked singles" found ' + CAST(@result as varchar(2))  + ' digits';
-		--exec sp_eliminateCandidates;
+		exec @result = sp_rule_nakedSingle;
+		if(@result<>-1) 
+		begin
+			select 'rule "naked singles" found ' + CAST(@result as varchar(2))  + ' digits';
+			set @hasFound=1;
+			--exec sp_eliminateCandidates;
+		end
 	end
+
+
+
+	set @result=0
+	while @result<>-1
+	begin
+		exec @result=sp_rule_hiddenSingles;
+		if(@result<>-1) 
+		begin
+			select 'rule "hidden single" found ' + CAST(@result as varchar(2))  + ' digits';
+			set @hasFound=1;
+			--exec sp_eliminateCandidates;
+		end
+	end
+
+
+	set @result=0
+	while @result<>-1
+	begin
+		exec @result=sp_rule_hiddenSingles2;
+		if(@result<>-1) 
+		begin
+			select 'rule "hidden single type 2" found ' + CAST(@result as varchar(2))  + ' digits';
+			set @hasFound=1;
+			--exec sp_eliminateCandidates;
+		end
+	end
+
+	set @result=0
+	while @result<>-1
+	begin
+		exec @result=sp_rule_hiddenSingles2;
+		if(@result<>-1) 
+		begin
+			select 'rule "hidden single type 2" found ' + CAST(@result as varchar(2))  + ' digits';
+			set @hasFound=1;
+			--exec sp_eliminateCandidates;
+		end
+	end
+
+
+	set @result=0
+	while @result<>-1
+	begin
+		exec @result=sp_rule_lockedCandidates_type1;
+		if(@result<>-1) 
+		begin
+			select 'rule "locked candidates type 1" marked of  ' + CAST(@result as varchar(2))  + ' candidates';
+			set @hasFound=1;
+		end
+	end
+
+
+	if((select count(*) from dbo.candidates as ca where ca.digit_current=0) = 0)
+		set @isSoln=1
+	else
+		set @isSoln=0;
 end
 
-
-
-set @result=0
-while @result<>-1
-begin
-	exec @result=sp_rule_hiddenSingles;
-	if(@result<>-1) 
-	begin
-		select 'rule "hidden single" found ' + CAST(@result as varchar(2))  + ' digits';
-		--exec sp_eliminateCandidates;
-	end
-end
-
-
-set @result=0
-while @result<>-1
-begin
-	exec @result=sp_rule_hiddenSingles2;
-	if(@result<>-1) 
-	begin
-		select 'rule "hidden single type 2" found ' + CAST(@result as varchar(2))  + ' digits';
-		--exec sp_eliminateCandidates;
-	end
-end
-
-set @result=0
-while @result<>-1
-begin
-	exec @result=sp_rule_hiddenSingles2;
-	if(@result<>-1) 
-	begin
-		select 'rule "hidden single type 2" found ' + CAST(@result as varchar(2))  + ' digits';
-		--exec sp_eliminateCandidates;
-	end
-end
-
-
-set @result=0
-while @result<>-1
-begin
-	exec @result=sp_rule_lockedCandidates_type1;
-	if(@result<>-1) 
-	begin
-		select 'rule "locked candidates type 1" marked of  ' + CAST(@result as varchar(2))  + ' candidates';
-		
-	end
-end
-
-
-
+if @isSoln=1 
+	select 'solution found';
+	else 
+	select 'no solution found';
 --select * from candidates
 
